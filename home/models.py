a3f2662f7dab121560d128c8d067d4f43cfad8f3
leprikon8888 <leprikon8888@gmail.com>
2024-02-07 14:11:32 +0200

    Initial commit

---
+++ home/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+
+class DishCategory(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    position = models.PositiveSmallIntegerField()
+    is_visible = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name_plural = 'Dish Categories'
+        ordering = ('position',)
+
+    def __iter__(self):
+        dishes = self.dishes.filter(is_visible=True)
+        for dish in dishes:
+            yield dish
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='url')
+    ingredients = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    photo = models.ImageField(upload_to='dishes/', blank=True)
+    is_visible = models.BooleanField(default=True)
+    position = models.PositiveSmallIntegerField()
+
+    category = models.ForeignKey(DishCategory, on_delete=models.PROTECT, related_name='dishes')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name_plural = 'Dishes'
+        ordering = ('position',)
+        constraints = [
+            models.UniqueConstraint(fields=['position', 'category'], name='unique_roder_per_each_category'),
+        ]
